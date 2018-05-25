346c9dd8e58a7bfc33822e7f5801d3ab03c7a11e
Chulps <chuckoward@gmail.com>
2018-05-25 09:47:27 -0700

    added dot background to california and nav ul components

--- client/src/components/Mainpage/Hero/California.js
+++ client/src/components/Mainpage/Hero/California.js
@@ -13,16 +13,17 @@ const California = () => {
         offset-md-0 
         text-center 
         pt-xs-5 
-        pt-md-0 
+        pt-md-5 
         order-last
         order-md-first 
         text-md-left 
         align-self-end 
         mb-5
+        bg-dot-grid
         '>
         <img className='d-block mx-auto mx-md-0' src='http://www.lillyandcompany.com/images/california.png' width={100} alt='California'/>
         <p className='d-inline font-weight-bold text-nowrap'>
-            Development. Design. Animation.
+            Web Design & Development.
             <br/>
             <small>Made with Love in California.</small>
         </p>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,14 +4,14 @@ const Navbar = () => {
   
   return (
     <div className="sticky-top">
-      <nav className="navbar navbar-dark bg-transparent bg-md-dark navbar-expand-lg">
+      <nav className="navbar navbar-dark bg-md-dark navbar-expand-lg">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navToggle" aria-controls="navToggle" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className='brand border border-white text-white p-2 text-center rounded'>Chuck Howard</div>
 
         <div className="collapse navbar-collapse" id="navToggle">
-          <ul className="navbar-nav ml-auto mt-2 text-center" data-toggle="collapse" data-target="#navToggle" aria-controls="navToggle" aria-expanded="false" aria-label="Toggle navigation">
+          <ul className="bg-dot-grid pt-1 pl-1 navbar-nav ml-auto mt-2 text-center" data-toggle="collapse" data-target="#navToggle" aria-controls="navToggle" aria-expanded="false" aria-label="Toggle navigation">
             
             <li className="nav-item">
               <a className="nav-link" href="#about">
